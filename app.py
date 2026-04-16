1b8da64834fe2ece699af66a5ae45472341f3636
vy3289192-a11y <vy3289192@gmail.com>
2026-04-16 17:34:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1230,19 +1230,19 @@ def home():
                 c.save()
                 pdf_io.seek(0)
                 return send_file(pdf_io, mimetype='application/pdf', as_attachment=True, download_name='id_card_print.pdf')
-                       if tool_type == 'sign':
+            if tool_type == 'sign':
                 file = request.files.get('file')
-                if not file:
-                    return "No file uploaded", 400
-                
+                if not file or not allowed_file(file.filename):
+                 return "Invalid or no file uploaded", 400
+
                 img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
                 coords = cv2.findNonZero(255 - thresh)
-                
+
                 if coords is None:
-                    return "Signature nahi detect hui. Clearer aur bright photo upload karein.", 400
-                
+                    return "Signature nahi detect hui. Clear aur bright photo upload karein.", 400
+
                 x, y, w, h = cv2.boundingRect(coords)
                 cropped = thresh[y:y+h, x:x+w]
                 cropped = cv2.copyMakeBorder(cropped, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=[255, 255, 255])
